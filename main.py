ae64ecd623f2f0d228ddfbdc56e0efea9f37369c
Isaac Wolf <ijwolf8@gmail.com>
2023-06-11 11:52:57 -0700

    feat: pre-cock snowflake

--- main.py
+++ main.py
@@ -19,48 +19,61 @@ def segments_intersect(A, B, C, D):
     return ((oriented_area(A, B, C) * oriented_area(A, B, D) <= 0) and
             (oriented_area(C, D, A) * oriented_area(C, D, B) <= 0))
 
-def generate_random_lines(num_lines, x_range, y_range, filename='random_lines.png'):
-    fig = go.Figure(layout=go.Layout(paper_bgcolor='black',
-                                      plot_bgcolor='black',
-                                      xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-                                      yaxis=dict(showgrid=False, zeroline=False, showticklabels=False)))
+def generate_random_lines(num_lines, x_range, y_range, filename="random_lines.png"):
+    fig = go.Figure(
+        layout=go.Layout(
+            paper_bgcolor="black",
+            plot_bgcolor="black",
+            xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+            yaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+        )
+    )
 
-    # List of line segments represented as tuple of two points
+    # Generate line lengths based on the golden ratio
+    lengths = [1 / (GOLDEN_RATIO ** i) for i in range(num_lines)]
+
+    # List of line segments represented as tuples of two points
     lines = []
 
     while len(lines) < num_lines:
-        # Choose randomly between a vertical and a horizontal line
-        if random.choice([True, False]):
-            # Vertical line: x-coordinates are equal, y-coordinates vary
-            x1 = x2 = random.uniform(x_range[0], x_range[1])
-            y1, y2 = random.uniform(y_range[0], y_range[1]), random.uniform(y_range[0], y_range[1])
-        else:
-            # Horizontal line: y-coordinates are equal, x-coordinates vary
-            y1 = y2 = random.uniform(y_range[0], y_range[1])
-            x1, x2 = random.uniform(x_range[0], x_range[1]), random.uniform(x_range[0], x_range[1])
+        x1 = random.uniform(x_range[0], x_range[1])
+        y1 = random.uniform(y_range[0], y_range[1])
+        
+        # Define x2 and y2 as functions of x1 and y1
+        x2 = random.uniform(x_range[0], x_range[1]) * GOLDEN_RATIO
+        y2 = random.uniform(y_range[0], y_range[1]) / GOLDEN_RATIO
 
         new_line = ((x1, y1), (x2, y2))
 
         # Check if the new line intersects with any existing line
-        if not any(segments_intersect(line[0], line[1], new_line[0], new_line[1]) for line in lines):
+        if not any(
+            segments_intersect(line[0], line[1], new_line[0], new_line[1]) for line in lines
+        ):
             lines.append(new_line)
 
     # Sort lines by length
     lines.sort(key=line_length)
 
-    # Calculate the gradient of colors using the golden ratio
-    gradient = [cm.plasma(i / GOLDEN_RATIO % 1) for i in range(num_lines)]
-
-    # Get the RGB values and format them as CSS-compatible strings
-    colorscale = ["rgb({}, {}, {})".format(int(r * 255), int(g * 255), int(b * 255)) for r, g, b, _ in gradient]
+    # Get color scale
+    colorscale = plotly.colors.n_colors(
+        "rgb(0, 200, 255)", "rgb(128, 0, 128)", len(lines), colortype="rgb"
+    )
 
     for i, line in enumerate(lines):
-        fig.add_trace(go.Scatter(x=[line[0][0], line[1][0]], y=[line[0][1], line[1][1]], 
-                                 mode='lines',
-                                 line=dict(color=colorscale[i], width=2)))
+        fig.add_trace(
+            go.Scatter(
+                x=[line[0][0], line[1][0]],
+                y=[line[0][1], line[1][1]],
+                mode="lines",
+                line=dict(color=colorscale[i], width=2),
+            )
+        )
 
-
-    fig.update_layout(template='plotly_dark', showlegend=False, title_text=fake.sentence(nb_words=4))
+    fig.update_layout(
+        template="plotly_dark",
+        showlegend=False,
+        title_text=fake.sentence(nb_words=4),
+    )
     fig.write_image(filename, height=1080, width=1920)
     fig.show()
 
@@ -74,7 +87,7 @@ def save_script(filename):
 
 if __name__ == '__main__':
 
-    iteration = 15
+    iteration = 18
     n_variations = 10
     for variation in range(0, n_variations):
         path = Path(f"NFT_{iteration:06d}") / f"{variation:02d}"
