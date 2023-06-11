b1eb0f5c954059de6de59a7512cd3769327ed935
Isaac Wolf <ijwolf8@gmail.com>
2023-06-11 11:24:53 -0700

    feat: genuis

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ def segments_intersect(A, B, C, D):
     return ((oriented_area(A, B, C) * oriented_area(A, B, D) <= 0) and
             (oriented_area(C, D, A) * oriented_area(C, D, B) <= 0))
 
-
 def generate_random_lines(num_lines, x_range, y_range, filename='random_lines.png'):
     fig = go.Figure(layout=go.Layout(paper_bgcolor='black',
                                       plot_bgcolor='black',
@@ -27,9 +26,16 @@ def generate_random_lines(num_lines, x_range, y_range, filename='random_lines.pn
     lines = []
 
     while len(lines) < num_lines:
-        # Generate two random points for each line
-        x1, x2 = random.uniform(x_range[0], x_range[1]), random.uniform(x_range[0], x_range[1])
-        y1, y2 = random.uniform(y_range[0], y_range[1]), random.uniform(y_range[0], y_range[1])
+        # Choose randomly between a vertical and a horizontal line
+        if random.choice([True, False]):
+            # Vertical line: x-coordinates are equal, y-coordinates vary
+            x1 = x2 = random.uniform(x_range[0], x_range[1])
+            y1, y2 = random.uniform(y_range[0], y_range[1]), random.uniform(y_range[0], y_range[1])
+        else:
+            # Horizontal line: y-coordinates are equal, x-coordinates vary
+            y1 = y2 = random.uniform(y_range[0], y_range[1])
+            x1, x2 = random.uniform(x_range[0], x_range[1]), random.uniform(x_range[0], x_range[1])
+
         new_line = ((x1, y1), (x2, y2))
 
         # Check if the new line intersects with any existing line
@@ -48,7 +54,7 @@ def generate_random_lines(num_lines, x_range, y_range, filename='random_lines.pn
                                  line=dict(color=colorscale[i], width=2)))
 
     fig.update_layout(template='plotly_dark', showlegend=False)
-    fig.write_image(filename, height=2000, width=2000)
+    fig.write_image(filename, height=1080, width=1920)
     fig.show()
 
 def save_script(filename):
@@ -61,12 +67,18 @@ def save_script(filename):
 
 if __name__ == '__main__':
 
-    iteration = 7
-    path = Path(f"NFT_{iteration:06d}")
-    path.mkdir(exist_ok=True)
+    iteration = 12
+    n_variations = 1
+    for variation in range(0, n_variations):
+        path = Path(f"NFT_{iteration:06d}") / f"{variation:02d}"
+        path.mkdir(parents=True, exist_ok=True)
 
-    # Use the function
-    generate_random_lines(50, [-10, 10], [-10, 10], filename=path / "image.png")
+        # Use the function
+        generate_random_lines(5000, [-10, 10], [-10, 10], filename=path / "image.png")
 
-    # Call the function with the filename you want to save the script as
-    save_script(path / f'script.py')
+        # Copy to working image
+        shutil.copy(path / "image.png", "working_image.png")
+
+        # Call the function with the filename you want to save the script as
+        save_script(path / f'script.py')
+
