00a27168b05b75f458d824529903a04663c05d96
Isaac Wolf <ijwolf8@gmail.com>
2023-06-11 10:52:34 -0700

    feat: up to gen 4 now bitch

--- main.py
+++ main.py
@@ -5,23 +5,37 @@ import shutil
 import os
 import sys
 
+def line_length(line):
+    return ((line[1][0] - line[0][0]) ** 2 + (line[1][1] - line[0][1]) ** 2) ** 0.5
+
+def oriented_area(A, B, C):
+    return (B[0] - A[0]) * (C[1] - A[1]) - (B[1] - A[1]) * (C[0] - A[0])
+
+def segments_intersect(A, B, C, D):
+    return ((oriented_area(A, B, C) * oriented_area(A, B, D) <= 0) and
+            (oriented_area(C, D, A) * oriented_area(C, D, B) <= 0))
+
 def generate_random_lines(num_lines, x_range, y_range, filename='random_lines.png'):
     fig = go.Figure()
 
-    for _ in range(num_lines):
-        # generate random slope and y-intercept
-        slope = random.uniform(-10, 10)
-        y_intercept = random.uniform(-10, 10)
+    # List of line segments represented as tuple of two points
+    lines = []
 
-        # generate x and y values for the line
-        x_values = [random.uniform(x_range[0], x_range[1]) for _ in range(100)]
-        y_values = [slope * x + y_intercept for x in x_values]
+    while len(lines) < num_lines:
+        # Generate two random points for each line
+        x1, x2 = random.uniform(x_range[0], x_range[1]), random.uniform(x_range[0], x_range[1])
+        y1, y2 = random.uniform(y_range[0], y_range[1]), random.uniform(y_range[0], y_range[1])
+        new_line = ((x1, y1), (x2, y2))
 
-        fig.add_trace(go.Scatter(x=x_values, y=y_values, mode='lines'))
-    
+        # Check if the new line intersects with any existing line
+        if not any(segments_intersect(line[0], line[1], new_line[0], new_line[1]) for line in lines):
+            lines.append(new_line)
+
+    for line in lines:
+        fig.add_trace(go.Scatter(x=[line[0][0], line[1][0]], y=[line[0][1], line[1][1]], mode='lines'))
+
     fig.write_image(filename)
     fig.show()
-
 
 def save_script(filename):
 
@@ -33,12 +47,12 @@ def save_script(filename):
 
 if __name__ == '__main__':
 
-    iteration = 1
+    iteration = 4
     path = Path(f"NFT_{iteration:06d}")
-    path.mkdir(parents=True)
+    path.mkdir(exist_ok=True)
 
     # Use the function
-    generate_random_lines(5, [-10, 10], [-10, 10], filename=path / "image.png")
+    generate_random_lines(50, [-10, 10], [-10, 10], filename=path / "image.png")
 
     # Call the function with the filename you want to save the script as
     save_script(path / f'script.py')
