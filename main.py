5fcc06c5528a24a956fc8e6f5e8e98019e396d95
Isaac Wolf <ijwolf8@gmail.com>
2023-06-11 13:51:10 -0700

    refactor: some great art

--- main.py
+++ main.py
@@ -9,6 +9,22 @@ import plotly
 from faker import Faker
 fake = Faker()
 GOLDEN_RATIO = (1 + 5 ** 0.5) / 2
+
+def generate_random_colors(num_colors):
+    colors = []
+    for _ in range(num_colors):
+        # Generate random RGB values between 0 and 255
+        red = random.randint(0, 255)
+        green = random.randint(0, 255)
+        blue = random.randint(0, 255)
+        
+        # Create the color string in the format "rgb(r, g, b)"
+        color = f"rgb({red}, {green}, {blue})"
+        
+        colors.append(color)
+    
+    return colors
+
 def line_length(line):
     return ((line[1][0] - line[0][0]) ** 2 + (line[1][1] - line[0][1]) ** 2) ** 0.5
 
@@ -30,7 +46,11 @@ def generate_random_lines(num_lines, x_range, y_range, filename='random_lines.pn
 
     # List of line segments represented as tuple of two points
     lines = []
-
+    
+    # Get color scale
+    colorscale = plotly.colors.n_colors('rgb(0, 200, 255)', 'rgb(128, 0, 128)', int(num_lines / 10), colortype='rgb')
+    colorscale = generate_random_colors(int(num_lines / 10))
+    color_idx = -1
     while len(lines) < num_lines:
         # Choose randomly between a vertical and a horizontal line
         if random.choice([True, False]):
@@ -42,22 +62,29 @@ def generate_random_lines(num_lines, x_range, y_range, filename='random_lines.pn
             y1 = y2 = random.uniform(y_range[0], y_range[1])
             x1, x2 = random.uniform(x_range[0], x_range[1] - lengths[len(lines)]), random.uniform(x_range[0] + lengths[len(lines)], x_range[1])
 
-        new_line = ((x1, y1), (x2, y2))
+        if len(lines) % 10 == 0:
+            new_line = ((x1, y1), (x2, y2))
+        else:
+            new_line = (lines[-1][1], (x2, y2))
 
         # Check if the new line intersects with any existing line
-        if not any(segments_intersect(line[0], line[1], new_line[0], new_line[1]) for line in lines):
+        if not any(segments_intersect(line[0], line[1], new_line[0], new_line[1]) for line in lines[:-1]):
+
             lines.append(new_line)
 
-    # Sort lines by length
-    lines.sort(key=line_length)
 
-    # Get color scale
-    colorscale = plotly.colors.n_colors('rgb(0, 200, 255)', 'rgb(128, 0, 128)', len(lines), colortype='rgb')
+    # Sort lines by length
+    # lines.sort(key=line_length)
+
 
     for i, line in enumerate(lines):
+                
+        if i % 10 == 0:
+            color_idx = color_idx + 1
+
         fig.add_trace(go.Scatter(x=[line[0][0], line[1][0]], y=[line[0][1], line[1][1]], 
                                  mode='lines',
-                                 line=dict(color=colorscale[i], width=2)))
+                                 line=dict(color=colorscale[color_idx], width=2)))
 
     fig.update_layout(template='plotly_dark', showlegend=False, title_text=fake.sentence(nb_words=4))
     fig.write_image(filename, height=1080, width=1920)
@@ -73,8 +100,8 @@ def save_script(filename):
 
 if __name__ == '__main__':
 
-    iteration = 15
-    n_variations = 10
+    iteration = 29
+    n_variations = 5
     for variation in range(0, n_variations):
         path = Path(f"NFT_{iteration:06d}") / f"{variation:02d}"
         path.mkdir(parents=True, exist_ok=True)
